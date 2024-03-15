3e622fbde256b0018769f7b54cb91a7e58392793
kobejean <s1292010@u-aizu.ac.jp>
2024-03-15 10:01:49 +0900

    u

---
+++ dl.sh
@@ -0,0 +1,9 @@
+
+nerfbaselines download-dataset scannerf/spinosaurus
+nerfbaselines download-dataset scannerf/stegosaurus
+nerfbaselines download-dataset scannerf/tiger
+nerfbaselines download-dataset scannerf/tractor
+nerfbaselines download-dataset scannerf/trex
+nerfbaselines download-dataset scannerf/triceratops
+nerfbaselines download-dataset scannerf/truck
+nerfbaselines download-dataset scannerf/zebra
